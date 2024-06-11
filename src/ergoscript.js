e5c185d834b9a817b1cdd4940b99e8610af81f54
c8e4 <c8e4@proton.me>
2024-06-11 22:52:24 +0200

    fix timelock

--- src/ergoscript.js
+++ src/ergoscript.js
@@ -1,63 +1,80 @@
-import * as Blockly from 'blockly/core';
+import * as Blockly from "blockly/core";
 
-export const ergoScriptGenerator = new Blockly.Generator('ErgoScript');
+export const ergoScriptGenerator = new Blockly.Generator("ErgoScript");
 
-ergoScriptGenerator.ORDER_ATOMIC =  0;
-ergoScriptGenerator.ORDER_LOGICAL_AND =1;
-ergoScriptGenerator.ORDER_LOGICAL_OR =  1;
+ergoScriptGenerator.ORDER_ATOMIC = 0;
+ergoScriptGenerator.ORDER_LOGICAL_AND = 1;
+ergoScriptGenerator.ORDER_LOGICAL_OR = 1;
 ergoScriptGenerator.ORDER_CONDITIONAL = 2;
 
-ergoScriptGenerator.init = function(workspace) {
-  // Initialize the generator, if needed.
+ergoScriptGenerator.init = function (workspace) {
+	// Initialize the generator, if needed.
 };
 
-ergoScriptGenerator.finish = function(code) {
-  // Finalize the generated code.
-  return code;
+ergoScriptGenerator.finish = function (code) {
+	// Finalize the generated code.
+	return code;
 };
 
 ergoScriptGenerator.forBlock = Object.create(null);
 
-ergoScriptGenerator.forBlock['public_key'] = function (block) {
-  const key = block.getFieldValue('key');
-  const code = `pk${key}`;
-  return [code, ergoScriptGenerator.ORDER_ATOMIC];
+ergoScriptGenerator.forBlock["public_key"] = function (block) {
+	const key = block.getFieldValue("key");
+	const code = `pk${key}`;
+	return [code, ergoScriptGenerator.ORDER_ATOMIC];
 };
 
-ergoScriptGenerator.forBlock['public_key_threshold'] = function (block) {
-  const key = block.getFieldValue('key');
-  const code = `pk${key}, `;
-  return code;
+ergoScriptGenerator.forBlock["public_key_threshold"] = function (block) {
+	const key = block.getFieldValue("key");
+	const code = `pk${key}, `;
+	return code;
 };
 
-ergoScriptGenerator.forBlock['and'] = function (block) {
-  const valueA = ergoScriptGenerator.valueToCode(block, 'A', ergoScriptGenerator.ORDER_ATOMIC);
-  const valueB = ergoScriptGenerator.valueToCode(block, 'B', ergoScriptGenerator.ORDER_ATOMIC);
-  const code = `${valueA} && ${valueB}`;
-  return [code, ergoScriptGenerator.ORDER_LOGICAL_AND];
+ergoScriptGenerator.forBlock["and"] = function (block) {
+	const valueA = ergoScriptGenerator.valueToCode(
+		block,
+		"A",
+		ergoScriptGenerator.ORDER_ATOMIC
+	);
+	const valueB = ergoScriptGenerator.valueToCode(
+		block,
+		"B",
+		ergoScriptGenerator.ORDER_ATOMIC
+	);
+	const code = `${valueA} && ${valueB}`;
+	return [code, ergoScriptGenerator.ORDER_LOGICAL_AND];
 };
 
-ergoScriptGenerator.forBlock['or'] = function (block) {
-  const valueA = ergoScriptGenerator.valueToCode(block, 'A', ergoScriptGenerator.ORDER_ATOMIC);
-  const valueB = ergoScriptGenerator.valueToCode(block, 'B', ergoScriptGenerator.ORDER_ATOMIC);
-  const code = `${valueA} || ${valueB}`;
-  return [code, ergoScriptGenerator.ORDER_LOGICAL_OR];
+ergoScriptGenerator.forBlock["or"] = function (block) {
+	const valueA = ergoScriptGenerator.valueToCode(
+		block,
+		"A",
+		ergoScriptGenerator.ORDER_ATOMIC
+	);
+	const valueB = ergoScriptGenerator.valueToCode(
+		block,
+		"B",
+		ergoScriptGenerator.ORDER_ATOMIC
+	);
+	const code = `${valueA} || ${valueB}`;
+	return [code, ergoScriptGenerator.ORDER_LOGICAL_OR];
 };
 
 ergoScriptGenerator.forBlock['timelock'] = function (block) {
-  const blockheight = block.getFieldValue('blockheight');
-  const statementsDo = ergoScriptGenerator.statementToCode(block, 'DO');
-  const statementsElse = ergoScriptGenerator.statementToCode(block, 'ELSE');
-  const code = `(blockheight <= ${blockheight} ? ${statementsDo} : ${statementsElse})`;
-  return [code, ergoScriptGenerator.ORDER_CONDITIONAL];
-};
+	const blockheight = block.getFieldValue('blockheight');
+	const valueDo = ergoScriptGenerator.valueToCode(block, 'DO', ergoScriptGenerator.ORDER_ATOMIC);
+	const valueElse = ergoScriptGenerator.valueToCode(block, 'ELSE', ergoScriptGenerator.ORDER_ATOMIC);
+	const code = `(blockheight <= ${blockheight} ? ${valueDo} : ${valueElse})`;
+	return [code, ergoScriptGenerator.ORDER_CONDITIONAL];
+  };
+  
 
-ergoScriptGenerator.forBlock['threshold'] = function (block) {
-  const min = block.getFieldValue('min');
-  const max = block.getFieldValue('max');
-  const statementsKeys = ergoScriptGenerator.statementToCode(block, 'KEYS');
-  const code = `threshold(${min}, ${max}, [${statementsKeys}])`;
-  return [code, ergoScriptGenerator.ORDER_ATOMIC];
+ergoScriptGenerator.forBlock["threshold"] = function (block) {
+	const min = block.getFieldValue("min");
+	const max = block.getFieldValue("max");
+	const statementsKeys = ergoScriptGenerator.statementToCode(block, "KEYS");
+	const code = `threshold(${min}, ${max}, [${statementsKeys}])`;
+	return [code, ergoScriptGenerator.ORDER_ATOMIC];
 };
 
 export const blocks = [
